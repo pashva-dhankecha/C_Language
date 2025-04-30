4778e6fb8d1b7793d25365632ffd9a2d7b616a89
pashva-dhankecha <pashvadhankecha1103@gmail.com>
2025-04-30 14:05:08 +0530

    while - loop

---
+++ lec-4/loop-4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int i,a;
+	print("enter the value of a :- ");
+	scanf("%d",&a);
+	
+	while(i>=)
+	{
+		printf("%d \n",i);
+		i=i+2;
+	}
+}
---
+++ lec-4/loop-5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int i=0;
+	
+	while(i<=20)
+	{
+		printf("%d \n",i);
+		i=i+5;
+	}
+}
